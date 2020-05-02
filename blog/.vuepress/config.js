4708bb0ee3babf79c759868eca475bf023d3af87
ydcjeff <ydcjeff@outlook.com>
2020-05-03 02:44:54 +0630

    init

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -3,14 +3,9 @@ module.exports = {
   description: "An Organization of Students doing Research and Development in Robotics and Artificial Intelligence",
   theme: "@vuepress/theme-blog",
   head: [
-    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'icon', href: '/android-chrome-512x512.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: [
     '@vuepress/active-header-links',
@@ -35,16 +30,10 @@ module.exports = {
         searchMaxSuggestions: 12
       }
     ],
-    [
-      '@vuepress/pwa',
-      {
-        serviceWorker: true,
-        updatePopup: true
-      }
-    ],
   ],
   themeConfig: {
     smoothScroll: true,
+    pwa: true,
     paginationComponent: 'SimplePagination',
     feed: {
       canonical_base: 'https://sharing.loratech.org',
