0076e3b16faf8d3b265c303c043229a7b348939b
ydcjeff <ydcjeff@outlook.com>
2020-05-03 02:16:23 +0630

    init

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,7 +1,17 @@
 module.exports = {
   title: "L.O.R.A Sharing Hub",
   description: "An Organization of Students doing Research and Development in Robotics and Artificial Intelligence",
-  theme: "@vuepress/theme-blog", // OR shortcut: @vuepress/blog
+  theme: "@vuepress/theme-blog",
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   plugins: [
     '@vuepress/active-header-links',
     "@vuepress/medium-zoom",
@@ -18,6 +28,13 @@ module.exports = {
       '@vuepress/search',
       {
         searchMaxSuggestions: 12
+      }
+    ],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
       }
     ],
   ],
