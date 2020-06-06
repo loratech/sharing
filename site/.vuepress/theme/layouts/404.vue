c008a0145bebb56317c646b281d0dad35c2d3f33
ydcjeff <ydcjeff@outlook.com>
2020-06-06 20:29:57 +0630

    fix(404, padding):404 page, padding wider

---
+++ site/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="root">
+    <h1>404</h1>
+    <blockquote>{{ getMsg() }}</blockquote>
+    <RouterLink to="/">Take me home.</RouterLink>
+    <img src="/error.svg" alt="Error 404" class="error">
+  </div>
+</template>
+
+<script>
+const msgs = [
+  `There's nothing here.`,
+  `How did we get here?`,
+  `That's a Four-Oh-Four.`,
+  `Looks like we've got some broken links.`,
+  `You have reached a Dead End.`,
+  `It's Empty here!`
+];
+export default {
+  methods: {
+    getMsg() {
+      return msgs[Math.floor(Math.random() * msgs.length)];
+    }
+  }
+};
+</script>
